11bf450a45ee7fd1eaf12151d17af4cc53c81405
jajsison <56822856+jajsison@users.noreply.github.com>
2023-01-05 23:11:56 +0800

    django part 7 templateview change

--- polls/views.py
+++ polls/views.py
@@ -10,6 +10,7 @@ from .models import Choice, Question
 class IndexView(TemplateView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
+    model = Question
 
     def get_queryset(self):
         """
@@ -21,42 +22,45 @@ class IndexView(TemplateView):
         ).order_by('-pub_date')[:5]
 
     def get_context_data(self, **kwargs):
-        context = super(IndexView, self).get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
 
         context['latest_question_list'] = self.get_queryset()
         return context
 
 
 class DetailView(TemplateView):
-    model = Question
     template_name = 'polls/detail.html'
+    model = Question
 
-   # def get_queryset(self):
-    #    """
-    #   Excludes any questions that aren't published yet.
-    #  """
-    # import pdb
-    # pdb.set_trace()
-    # return Question.objects.get(pub_date__lte=timezone.now())
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.get(pub_date__lte=timezone.now())
 
     def get_context_data(self, **kwargs):
-        context = super(DetailView, self).get_context_data(**kwargs)
-        # import pdb
-        # pdb.set_trace()
+        context = super().get_context_data(**kwargs)
         context['question'] = Question.objects.get(id=self.kwargs['pk'])
 
         return context
 
 
 class ResultsView(TemplateView):
-    model = Question
     template_name = 'polls/results.html'
+    model = Question
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['question'] = Question.objects.get(
+            id=self.kwargs['question_id'])
+        return context
 
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        selected_choice = question.choice_set.get(
+            pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
@@ -66,7 +70,7 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        # # Get the votes for the selected choice
+        # votes = selected_choice.votes
+        # # Return the votes in the response
+        # return render(request, 'polls/results.html', {'votes': votes})
